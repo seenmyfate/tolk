7395c77f7e78b0bcbb3a3d3e2fbd0fbbaff203b2
seenmyfate <seenmyfate@gmail.com>
2011-03-31 09:17:24 +0100

    match authentication

--- app/controllers/tolk/application_controller.rb
+++ app/controllers/tolk/application_controller.rb
@@ -2,13 +2,14 @@ module Tolk
   class ApplicationController < ActionController::Base
     helper :all
     protect_from_forgery
-    
+
     cattr_accessor :authenticator
+    #before_filter :authenticate
     before_filter :authenticate_user!
 
-    def authenticate_user!
-      self.authenticator.bind(self).call if self.authenticator && self.authenticator.respond_to?(:call)
-    end
+    # def authenticate
+    #       self.authenticator.bind(self).call if self.authenticator && self.authenticator.respond_to?(:call)
+    #     end
 
     def ensure_no_primary_locale
       redirect_to tolk_locales_path if @locale.primary?
